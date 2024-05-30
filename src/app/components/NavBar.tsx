6905ec30818b8f3bfd23cb9069874299b1c8c68c
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-30 20:25:06 +0100

    feat: add background image

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <Image src="" alt="" />
+      
+    </div>
+  );
+}
+
+export default NavBar;
