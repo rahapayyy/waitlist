b0013d5a8db4f0f3acadde198d54a1ea8486ede6
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-31 13:48:07 +0100

    feat: add community link

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import React from "react";
 import logo from "../../../public/images/logo.png";
+import Link from "next/link";
 
 function NavBar() {
   return (
@@ -10,7 +11,13 @@ function NavBar() {
         <Image src={logo} alt="Logo" width={150} height={50} />
       </div>
       <Button className="py-2 px-4 md:py-4 md:px-6 bg-[#000] text-white">
-        Join Our Community
+        <Link
+          href={"https://chat.whatsapp.com/CSLZVVw7UovDv65jhBA3DZ"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join Our Community
+        </Link>
       </Button>
     </nav>
   );
